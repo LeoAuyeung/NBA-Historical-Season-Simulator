5e7ba916dbb470d737342709cf74e6f4789980f1
virtualboi <Leman.Yan80@myhunter.cuny.edu>
2020-05-16 15:57:06 -0400

    Implement knn in train model

--- train_model.py
+++ train_model.py
@@ -4,11 +4,92 @@ from sklearn import metrics
 import pandas as pd
 import pickle
 
+from sklearn.neighbors import KNeighborsClassifier
+
 import os
 
 from datetime import datetime
 
 home_path = os.getcwd()
+
+#k nearest neighbors
+def knn(dataframe):
+
+    # Features currently present within CSV data file: W_PCT,REB,TOV,PLUS_MINUS,OFF_RATING,DEF_RATING,TS_PCT
+    features = ['W_PCT', 'REB', 'TOV', 'PLUS_MINUS', 'OFF_RATING', 'DEF_RATING', 'TS_PCT']
+
+    # ==================== START store necessary variables ====================
+
+    # feature_data holds all features of W_PCT,REB,TOV,PLUS_MINUS,OFF_RATING,DEF_RATING,TS_PCT,
+    feature_data = dataframe[features]
+
+    # actual_result_data holds actual result of the games which we can then check our prediction with
+    actual_result_data = dataframe.Result
+
+    # Call sklearn.model_selection's train_test_split function: https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+    # Split arrays or matrices into random train and test subsets
+    X_train, X_test, Y_train, Y_test = train_test_split(feature_data, actual_result_data, test_size=0.25, shuffle=True)
+
+    # ==================== END store necessary variables ====================
+
+
+    # ==================== START applying k nearest neighbors ====================
+
+    #Call KNeighborsClassifier from https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
+    knn_result = KNeighborsClassifier(n_neighbors=3)
+
+    # Fit the model according to the given training data.
+    knn_result.fit(X_train, Y_train);
+
+    # Predict class labels for samples in X
+    Y_pred = knn_result.predict(X_test)
+
+    # Call sklearn's metric's confusion_matrix function: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html
+    # Compute confusion matrix to evaluate the accuracy of a classification
+    confusion_matrix = metrics.confusion_matrix(Y_test, Y_pred)
+
+    # ==================== END applying k nearest neighbors ====================
+
+
+    # ==================== Print model accuracy information ====================
+   
+    '''
+    K nearest neigbors doesnt seem to have any features
+    print('\nCoefficient Information: \n')
+
+    print('knn_result.classes_',knn_result.classes_)
+    print('knn_result.effective_metric_', knn_result.effective_metric_)
+    print('knn_result.effective_metric_params_', knn_result.effective_metric_params_)
+    print('knn_result.outputs_2d_', knn_result.outputs_2d_)
+    '''
+
+    # Loop through each feature
+    '''
+    for i in range(len(features)):  # Prints each feature next to its corresponding coefficient in the model
+
+        # Get feature name and corresponding coefficients
+        knn_coefficients = knn_result.coef_
+        curr_feature = features[i]
+        curr_coefficient = knn_coefficients[0][i]
+
+        # Print them
+        print(curr_feature + ': ' + str(curr_coefficient))
+
+    print('\n----------------------------------')
+    '''
+
+    # Printing accuracy, precision, and recall based on metrics data
+    print("Accuracy: ", metrics.accuracy_score(Y_test, Y_pred))
+    print("Precision: ", metrics.precision_score(Y_test, Y_pred))
+    print("Recall: ", metrics.recall_score(Y_test, Y_pred))
+
+    print('----------------------------------\n')
+
+    # Print confusion matrix
+    print('Confusion Matrix:')
+    print(confusion_matrix)
+
+    return knn_result
 
 def logistic_regression(dataframe):
 
@@ -90,14 +171,22 @@ def create_model(name="model"):
     all_games_dataframe = pd.read_csv('COMBINEDgamesWithInfo2016-19.csv')
 
     # Train logistic regression model based on the dataframe given by CSV
-    logistic_regression_model = logistic_regression(all_games_dataframe)
+    #logistic_regression_model = logistic_regression(all_games_dataframe)
+
+    # Train knn model 
+    knn_model = knn(all_games_dataframe);
 
     # Set directory to SavedModels
     os.chdir(home_path + '/SavedModels')
 
+    '''
     # Save Model
     with open(filename, 'wb') as file:
         pickle.dump(logistic_regression_model, file)
+    '''
+    # Save Model
+    with open(filename, 'wb') as file:
+        pickle.dump(knn_model, file)
 
 if __name__ == "__main__":
     create_model()
