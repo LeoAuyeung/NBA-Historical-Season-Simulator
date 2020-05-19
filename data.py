653e0d36748daef64cfbca9a3d6288966d76cf76
LeoAuyeung <leo297747@gmail.com>
2020-05-19 03:17:09 -0400

    Cleaned up and optimized data.py

--- data.py
+++ data.py
@@ -13,7 +13,8 @@ home_path = os.getcwd()
 
 # Get the normalized Z-scores of the games played based on the home and away team stats, mean, and std dev
 def get_z_scores_list(game, mean_dict, std_dev_dict, startDate, endDate, season):
-    home_team, away_team = list(game.items())[0]
+    home_team = list(game.items())[0]
+    away_team = list(game.items())[0]
     games = [home_team["label"], away_team["label"]]
 
 	home_stats = get_team_stats(home_team["name"], home_team["start_date"], home_team["end_date"], home_team["season"], use_cached_stats)
@@ -26,8 +27,8 @@ def get_z_scores_list(game, mean_dict, std_dev_dict, startDate, endDate, season)
 	return [games]
 
 # Get the data and the Zscore differentials of each game
-def get_data(game, season, startOfSeason, currentDate):
-    data_dicts = create_mean_std_dev_dicts(startOfSeason, currentDate, season)
+def get_data(game, season, start_of_season, current_date):
+    data_dicts = create_mean_std_dev_dicts(start_of_season, current_date, season)
     if not data_dicts or not data_dicts[0] or not data_dicts[1]: # If we got no data
         empty = pd.DataFrame()
         return empty
@@ -35,7 +36,7 @@ def get_data(game, season, startOfSeason, currentDate):
     std_dev_dict = data_dicts[1]
 
     # Call get Zscore helper function
-    games = get_z_scores_list(game, mean_dict, std_dev_dict, startOfSeason, currentDate, season)
+    games = get_z_scores_list(game, mean_dict, std_dev_dict, start_of_season, current_date, season)
     # Use Pandas dataframe to hold
     game_with_z_score_difs = pd.DataFrame(
 		games,
@@ -50,13 +51,13 @@ def main():
         print("Wrong usage")
         sys.exit()
     else:
-        seasonYear = int(sys.argv[1])
-    # seasonYear = 2011
-    gamefinder = leaguegamelog.LeagueGameLog(season=seasonYear, season_type_all_star=SeasonTypeAllStar.default)
+        season_year = int(sys.argv[1])
+    # season_year = 2011
+    gamefinder = leaguegamelog.LeagueGameLog(season=season_year, season_type_all_star=SeasonTypeAllStar.default)
     games = gamefinder.get_data_frames()[0]
-    prevTeam = games['TEAM_NAME'][0]
-    startOfSeason = games['GAME_DATE'][0]
-    season = startOfSeason[:4] + '-' + str(int(startOfSeason[2:4])+1)
+    previous_team = games['TEAM_NAME'][0]
+    start_of_season = games['GAME_DATE'][0]
+    season = start_of_season[:4] + '-' + str(int(start_of_season[2:4])+1)
 
     # download_helper_DF = pd.read_csv(home_path+'/Data/gamesWithInfo' + season + '.csv')
     # # If for some reason your program stopped running, continue progress by 
@@ -78,11 +79,11 @@ def main():
         if index <= last_index_downloaded:
             continue
         if index % 2 != 0:
-            currTeam = row['TEAM_NAME']
-            game = {prevTeam: currTeam}
+            current_team = row['TEAM_NAME']
+            game = {previous_team: current_team}
             game_date = row['GAME_DATE']
 
-            game_stat = get_data(game, season, startOfSeason, game_date)
+            game_stat = get_data(game, season, start_of_season, game_date)
             if game_stat.empty: # If we got no data
                 continue
             winLoss = row['WL']
@@ -99,7 +100,7 @@ def main():
 
             percent_done = (index + 1) / games.shape[0] # Total number of games
             percent_done *= 100
-            print('['+str(index)+'] Progress: ' + str(round(percent_done, 2)) + '%', " | ", 'Season ' + season, prevTeam + ' vs ' + currTeam)
+            print('['+str(index)+'] Progress: ' + str(round(percent_done, 2)) + '%', " | ", 'Season ' + season, previous_team + ' vs ' + current_team)
 
             if index == 1:
                 # Initialize the file with headers. This function resets the file.
@@ -112,7 +113,7 @@ def main():
                 with open(home_path+'/Data/gamesWithInfo'+season+'.csv','a') as file:
                     game_stat.to_csv(file, header=False)
         else:
-            prevTeam = row['TEAM_NAME']
+            previous_team = row['TEAM_NAME']
 
 if __name__ == "__main__":
 	main()
