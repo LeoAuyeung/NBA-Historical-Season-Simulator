35a822c9e958ea04796999f76ae96483bd02d546
Peter Lee <peterdjlee@gmail.com>
2020-05-19 14:00:18 -0400

    Add back the classification algorithms

--- AI_algorithms/train_model.py
+++ AI_algorithms/train_model.py
@@ -1,24 +1,79 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.ensemble import RandomForestClassifier
-from sklearn import metrics
-import pandas as pd
-import pickle
+from sklearn.tree import DecisionTreeClassifier
+from sklearn import metrics, datasets
 
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+import pandas as pd
+import pickle
 import os
-
 from datetime import datetime
 
-from sklearn.feature_selection import SelectKBest, VarianceThreshold
-from sklearn.feature_selection import f_classif
+from sklearn.feature_selection import SelectKBest, f_classif
 
 home_path = os.getcwd()
 
+
+#k nearest neighbors
+def knn(dataframe):
+
+	# Features currently present within CSV data file: W_PCT,REB,TOV,PLUS_MINUS,OFF_RATING,DEF_RATING,TS_PCT
+	features = ['W_PCT', 'REB', 'TOV', 'PLUS_MINUS', 'OFF_RATING', 'DEF_RATING', 'TS_PCT']
+
+	# ==================== START store necessary variables ====================
+
+	# feature_data holds all features of W_PCT,REB,TOV,PLUS_MINUS,OFF_RATING,DEF_RATING,TS_PCT,
+	feature_data = dataframe[features]
+
+	# actual_result_data holds actual result of the games which we can then check our prediction with
+	actual_result_data = dataframe.Result
+
+	# Call sklearn.model_selection's train_test_split function: https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+	# Split arrays or matrices into random train and test subsets
+	X_train, X_test, Y_train, Y_test = train_test_split(feature_data, actual_result_data, test_size=0.25, shuffle=True)
+
+	# ==================== END store necessary variables ====================
+
+
+	# ==================== START applying k nearest neighbors ====================
+
+	#Call KNeighborsClassifier from https://scikit-learn.org/stable/modules/generated/sklearn.neighbors.KNeighborsClassifier.html
+	knn_result = KNeighborsClassifier(n_neighbors=2)
+	print('n_neighbors = 2')
+
+	# Fit the model according to the given training data.
+	knn_result.fit(X_train, Y_train);
+
+	# Predict class labels for samples in X
+	Y_pred = knn_result.predict(X_test)
+
+	# Call sklearn's metric's confusion_matrix function: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html
+	# Compute confusion matrix to evaluate the accuracy of a classification
+	confusion_matrix = metrics.confusion_matrix(Y_test, Y_pred)
+
+	# ==================== END applying k nearest neighbors ====================
+
+
+	# ==================== Print model accuracy information ====================
+	# Printing accuracy, precision, and recall based on metrics data
+	print("Accuracy: ", metrics.accuracy_score(Y_test, Y_pred))
+	print("Precision: ", metrics.precision_score(Y_test, Y_pred))
+	print("Recall: ", metrics.recall_score(Y_test, Y_pred))
+
+	print('----------------------------------\n')
+
+	# Print confusion matrix
+	print('Confusion Matrix:')
+	print(confusion_matrix)
+
+	return knn_result
+   
+
 def random_forest(dataframe):
-    print(dataframe)
      # Features currently present within CSV data file: W_PCT,REB,TOV,PLUS_MINUS,OFF_RATING,DEF_RATING,TS_PCT
      # Total features
     features = ['W_PCT','MIN','FGM','FGA','FG_PCT','FG3M','FG3A','FG3_PCT','FTM','FTA','FT_PCT','OREB','DREB','REB','AST','TOV','STL','BLK','BLKA',
@@ -26,18 +81,23 @@ def random_forest(dataframe):
         'AST_RATIO','OREB_PCT','DREB_PCT','REB_PCT','TM_TOV_PCT','EFG_PCT','TS_PCT','E_PACE','PACE','PACE_PER40','POSS','PIE',]
     
     # Top 20 select KBest
-    features = ['W_PCT','FGM','FG_PCT','DREB','AST',
-    'BLKA','PTS','PLUS_MINUS','E_OFF_RATING','OFF_RATING',
-    'E_DEF_RATING','DEF_RATING','E_NET_RATING','NET_RATING','AST_TO',
-        'AST_RATIO','EFG_PCT','TS_PCT','PIE', 'FG3_PCT']
+    # features = ['W_PCT','FGM','FG_PCT','DREB','AST',
+    # 'BLKA','PTS','PLUS_MINUS','E_OFF_RATING','OFF_RATING',
+    # 'E_DEF_RATING','DEF_RATING','E_NET_RATING','NET_RATING','AST_TO',
+    #     'AST_RATIO','EFG_PCT','TS_PCT','PIE', 'FG3_PCT']
     
-    # Feature set 1
-    features = ['W_PCT', 'REB', 'TOV', 'PLUS_MINUS', 'OFF_RATING', 'DEF_RATING', 'TS_PCT']
+    # Original Feature Set
+    # features = ['W_PCT', 'REB', 'TOV', 'PLUS_MINUS', 'OFF_RATING', 'DEF_RATING', 'TS_PCT']
+
+    # Top 10 feature importance
+    features = ['W_PCT','NET_RATING','PLUS_MINUS','PIE','E_NET_RATING','DEF_RATING','E_OFF_RATING','OFF_RATING','PTS', 'TS_PCT']
+
+    # Top 10 K-best 
+    # features = ['W_PCT','NET_RATING','PLUS_MINUS','E_NET_RATING','PIE','E_OFF_RATING','PTS','OFF_RATING','TS_PCT','E_DEF_RATING']
     # Feature set 2
-    features = ['W_PCT','NET_RATING','PIE','PLUS_MINUS', 'DEF_RATING', 'TS_PCT', 'PTS']
+    # features = ['W_PCT','NET_RATING','PIE','PLUS_MINUS', 'DEF_RATING', 'TS_PCT', 'PTS']
 
     # ==================== START store necessary variables ====================
-
 
     # feature_data holds all features of W_PCT,REB,TOV,PLUS_MINUS,OFF_RATING,DEF_RATING,TS_PCT,
     feature_data = dataframe[features]
@@ -59,12 +119,6 @@ def random_forest(dataframe):
     X_train, X_test, Y_train, Y_test = train_test_split(feature_data, actual_result_data, test_size=0.3, shuffle=True)
 
     # ==================== END store necessary variables ====================
-
-    
-    sel_variance_threshold = VarianceThreshold() 
-    X_train_remove_variance = sel_variance_threshold.fit_transform(X_train)
-    print(X_train_remove_variance.shape)
-
 
     # ==================== START applying random forest ====================
     # Create a Gaussian Classifier
@@ -114,6 +168,130 @@ def random_forest(dataframe):
 
     return random_forest_result
 
+def decision_tree(dataframe):
+	 # Features currently present within CSV data file: W_PCT,REB,TOV,PLUS_MINUS,OFF_RATING,DEF_RATING,TS_PCT
+	features = ['W_PCT', 'REB', 'TOV', 'PLUS_MINUS', 'OFF_RATING', 'DEF_RATING', 'TS_PCT']
+
+	# feature_data holds all features of W_PCT,REB,TOV,PLUS_MINUS,OFF_RATING,DEF_RATING,TS_PCT,
+	feature_data = dataframe[features]
+
+	# actual_result_data holds actual result of the games which we can then check our prediction with
+	actual_result_data = dataframe.Result
+
+	# Call sklearn.model_selection's train_test_split function: https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+	# Split arrays or matrices into random train and test subsets
+	X_train, X_test, Y_train, Y_test = train_test_split(feature_data, actual_result_data, test_size=0.25, shuffle=True)
+
+	# Call sklearn.linear_model's Logistic Regression function: https://scikit-learn.org/stable/modules/generated/sklearn.linear_model.LogisticRegression.html
+	decision_tree_result = DecisionTreeClassifier()
+
+	# Fit the model according to the given training data.
+	decision_tree_result.fit(X_train, Y_train)
+
+	# Predict class labels for samples in X
+	Y_pred = decision_tree_result.predict(X_test)
+
+	# Call sklearn's metric's confusion_matrix function: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html
+	# Compute confusion matrix to evaluate the accuracy of a classification
+	confusion_matrix = metrics.confusion_matrix(Y_test, Y_pred)
+
+	print('\n----------------------------------')
+
+	# Printing accuracy, precision, and recall based on metrics data
+	print("Accuracy: ", metrics.accuracy_score(Y_test, Y_pred))
+	print("Precision: ", metrics.precision_score(Y_test, Y_pred))
+	print("Recall: ", metrics.recall_score(Y_test, Y_pred))
+
+	print('----------------------------------\n')
+
+	# Print confusion matrix
+	print('Confusion Matrix:')
+	print(confusion_matrix)
+
+	return decision_tree_result
+
+
+def gaussian_nb(dataframe):
+
+    # Features currently present within CSV data file: W_PCT,REB,TOV,PLUS_MINUS,OFF_RATING,DEF_RATING,TS_PCT
+    features = ['W_PCT', 'REB', 'TOV', 'PLUS_MINUS', 'OFF_RATING', 'DEF_RATING', 'TS_PCT']
+
+    # ==================== START store necessary variables ====================
+
+    # feature_data holds all features of W_PCT,REB,TOV,PLUS_MINUS,OFF_RATING,DEF_RATING,TS_PCT,
+    feature_data = dataframe[features]
+
+    # actual_result_data holds actual result of the games which we can then check our prediction with
+    actual_result_data = dataframe.Result
+
+    # Call sklearn.model_selection's train_test_split function: https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.train_test_split.html
+    # Split arrays or matrices into random train and test subsets
+    X_train, X_test, Y_train, Y_test = train_test_split(feature_data, actual_result_data, test_size=0.25, shuffle=True)
+
+    # ==================== END store necessary variables ====================
+
+
+    # ==================== START applying Gaussian NB ====================
+
+    # Call sklearn.naive_bayes's GaussianNB function: https://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.GaussianNB.html
+    result = GaussianNB()
+
+    # Fit the model according to the given training data.
+    result.fit(X_train, Y_train)
+
+    # Predict class labels for samples in X
+    Y_pred = result.predict(X_test)
+
+    # Call sklearn's metric's confusion_matrix function: https://scikit-learn.org/stable/modules/generated/sklearn.metrics.confusion_matrix.html
+    # Compute confusion matrix to evaluate the accuracy of a classification
+    confusion_matrix = metrics.confusion_matrix(Y_test, Y_pred)
+
+    # ==================== END applying Gaussian NB ====================
+
+
+    # ==================== Print model accuracy information ====================
+    print('\Sigma (variance) Information: \n')
+
+    # Loop through each feature
+    for i in range(len(features)):  # Prints each feature next to its corresponding coefficient in the model
+
+        # Get feature name and corresponding sigmas
+        sigmas = result.sigma_
+        curr_feature = features[i]
+        curr_coefficient = sigmas[0][i]
+
+        # Print them
+        print(curr_feature + ': ' + str(curr_coefficient))
+
+    print('\n----------------------------------')
+
+    print('\Theta (mean) Information: \n')
+
+    # Loop through each feature
+    for i in range(len(features)):  # Prints each feature next to its corresponding coefficient in the model
+
+        # Get feature name and corresponding thetas
+        thetas = result.theta_
+        curr_feature = features[i]
+        curr_coefficient = thetas[0][i]
+
+        # Print them
+        print(curr_feature + ': ' + str(curr_coefficient))
+
+    print('\n----------------------------------')
+
+    # Printing accuracy, precision, and recall based on metrics data
+    print("Accuracy: ", metrics.accuracy_score(Y_test, Y_pred))
+    print("Precision: ", metrics.precision_score(Y_test, Y_pred))
+    print("Recall: ", metrics.recall_score(Y_test, Y_pred))
+
+    print('----------------------------------\n')
+
+    # Print confusion matrix
+    print('Confusion Matrix:')
+    print(confusion_matrix)
+
+    return result
 
 
 def logistic_regression(dataframe):
@@ -196,29 +374,43 @@ def logistic_regression(dataframe):
     # plt.show()
 
     return log_reg_result
-    
+    def create_model_helper(df, model_name):
+	if model_name == "log_reg":
+		model = logistic_regression(df)
+	elif model_name == "knn":
+		model = knn(df)
+	elif model_name == "random_forest":
+		model = random_forest(df)
+	elif model_name == "dTree":
+		model = decision_tree(df)
+	elif model_name == "gaussian_nb":
+		model = gaussian_nb(df)
+	
+	return model
+
 # Create new training model and save after training
-def create_model(name="random_forest_model"):
-    now = datetime.now()
-    now_str = now.strftime("%Y%m%d")
+def create_model(name="model"):
+	now = datetime.now()
+	now_str = now.strftime("%Y%m%d")
 
-    filename = f'{name}_{now_str}.pkl'
+	# add svm here
+	model_names = ["log_reg", "knn", "random_forest", "dTree", "gaussian_nb"]
+	model_name=model_names[3]
 
-    # Set directory to Data
-    os.chdir(home_path + '/Data')
+	filename = f'{name}_{model_name}_{now_str}.pkl'
 
-    all_games_dataframe = pd.read_csv(home_path + '/Data/MoreInfoData/COMBINEDgamesWithMoreInfo2010-15.csv')
+	# Set directory to Data
+	os.chdir(home_path + '/Data')
+	all_games_dataframe = pd.read_csv('COMBINEDgamesWithInfo2016-19.csv')
 
-    # Train logistic regression model based on the dataframe given by CSV
-    # logistic_regression_model = logistic_regression(all_games_dataframe)
-    random_forest_model = random_forest(all_games_dataframe)
+	model = create_model_helper(all_games_dataframe, model_name)
 
-    # Set directory to SavedModels
-    os.chdir(home_path + '/SavedModels')
+	# Set directory to SavedModels
+	os.chdir(home_path + '/SavedModels')
 
-    # Save Model
-    with open(filename, 'wb') as file:
-        pickle.dump(random_forest_model, file)
+	# Save Model
+	with open(filename, 'wb') as file:
+		pickle.dump(model, file)
 
 if __name__ == "__main__":
-    create_model()
+	create_model()
